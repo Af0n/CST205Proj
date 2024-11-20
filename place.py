2f97edb2803fda3896a167d8b7f189ff105dd38f
AndAgGit <afon.cyffrous@gmail.com>
2024-11-20 15:51:37 -0800

    added __init__ methid

--- place.py
+++ place.py
@@ -5,3 +5,11 @@ class Place():
     webLink = ""
     name = ""
     imgSrc = ""
+    
+    def __init__(self, phone, address, maps, web, name, src):
+        self.phone = phone
+        self.address = address
+        self.mapsLink = maps
+        self.webLink = web
+        self.name = name
+        self.imgSrc = src
