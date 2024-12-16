1aae546b522f5c1dc8da8c046dae7f7a5e512772
Alyosha-ctrl <alexeibere@gmail.com>
2024-12-16 15:09:38 -0800

    Created details page, and hooked index.html up to it

--- app/flasking.py
+++ app/flasking.py
@@ -10,12 +10,20 @@ from api_calls import *
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+#Azrael To-Do get a function that calls thez
+places = text_search_from(36.663024, -121.769599, 10, 5000)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', locations = places)
+
+
+@app.route('/detail/<int:index>',methods=['GET'])
+def inspect():
+    #Azrael, takes in t
+    return render_template('placetest.html', place = places[index])
 
 @app.route('/placetest')
 def place_test():
     # temporary location latitude/longitude
-    list = text_search_from(36.663024, -121.769599, 10, 5000)
-    return render_template('placetest.html', places = list)
+    return render_template('placetest.html', location = places)
