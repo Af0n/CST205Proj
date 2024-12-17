e23b04ee78470c7cd0860ccb0cba788447523f60
AndAgGit <afon.cyffrous@gmail.com>
2024-12-16 19:03:05 -0800

    made following the search post action easier to follow and error handle

--- app/flasking.py
+++ app/flasking.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 from place import Place
@@ -15,32 +15,43 @@ app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
 
 place_list = []
-place_list = text_search_from(36.663024, -121.769599, 10, 5000)
+message = ""
 
 class searchInfo(FlaskForm):
     address = StringField(
         'Address', 
         validators=[DataRequired()]
     )
-    range = StringField(
+    range = IntegerField(
         'Range(in meters)', 
         validators=[DataRequired()]
     )
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    global message
+    message = ""
     if(len(place_list) < 1):
         return redirect('/search')
     return render_template('index.html', locations = place_list)
 
-@app.route('/search', methods=('GET', 'POST'))
+@app.route('/search')
 def search():
         sI = searchInfo()
-        if sI.validate_on_submit():
-            print("=====" + str(sI.address) + " " + str(sI.range) + "=====")
-            # converter(sI.address, sI.range)
-            return redirect('/index')
-        return render_template('search.html', form = sI)
+        return render_template('search.html', form = sI, msg = message)
+
+@app.route('/postsearch', methods=('GET', 'POST'))
+def post_search():
+    sI = searchInfo()
+    if sI.validate_on_submit():
+        print("=====" + str(sI.address) + " " + str(sI.range) + "=====")
+        global place_list
+        place_list = converter(sI.address.data, sI.range.data)
+        if(len(place_list) > 0):
+            return redirect('/')
+        global message
+        message = "Address too broad. Please enter more information such as a city or ZIP code."
+        return redirect('/search')
 
 
 @app.route('/detail/<int:index>')
